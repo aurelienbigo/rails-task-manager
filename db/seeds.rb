117e48118ce02812c48e2c34729394f6ea459a32
aurelien bigo <aurelien.bigo@gmail.com>
2017-08-08 12:59:43 +0200

    task management

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Task.create(name: 'Appeler Jocelyne', detail: 'prendre rdv pour négocier les prets')
+Task.create(name: 'Appeler Bob', detail: 'Il faut le virer asap')
+Task.create(name: 'Appeler John', detail: 'prendre rdv pour négocier les assurance')
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
